447c7a34228bd560f95fc7bf186374a8642d5816
snicoll3 <99807390+snicoll3@users.noreply.github.com>
2022-12-19 22:13:25 -0300

    PDFtk Atualizado

--- PDFtk/main.sh
+++ PDFtk/main.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 while : ; do
     options=$(yad     --list --fixed --center --borders=10 --window-icon="./images/panda.png" --title="Pandinha PDFTools" --columns=2 --no-buttons --image-on-top \
     --image=./images/panda.png                             \
@@ -8,9 +9,8 @@ while : ; do
                         1 "Escolher documentos para Editar" \
                         2 "Exibir Preview" \
                         3 "Dividir documento" \
-                        4 "Dividir um documento por página" \
-                        5 "Juntar documentos" \
-                        6 "Excluir Páginas" \
+                        4 "Juntar documentos" \
+                        5 "Excluir Páginas" \
                         0 "Sair" )
 
         options=$(echo $options | egrep -o '^[0-9]')
@@ -18,7 +18,7 @@ while : ; do
         # De acordo com a opção escolhida, dispara programas
         case "$options" in
             "0") exit;;
-            "1") FORM=$(
+            "1")    FORM=$(
                 yad --center --title="Selecione o local do Arquivo "            \
                     --width=400 --heigth=400                                    \
                     --form                                                      \
@@ -26,8 +26,19 @@ while : ; do
                     --field="PDF-2  : " ""                                       \
                     --button="Adicionar"                 \
                     )
-                PDF-1=$(echo "$FORM" | cut -d "|" -f 1)
-                PDF-2=$(echo "$FORM" | cut -d "|" -f 1)
+                PDF1=$(echo "$FORM" | cut -d "|" -f 1 )
+                PDF2=$(echo "$FORM" | cut -d "|" -f 2 )
                 echo $FORM;;
+            "2") echo "404 Not Found";;
+            "3")    BURST=$(
+                yad --center --title="Selecione o arquivo a ser dividido "            \
+                    --width=400 --heigth=400                                    \
+                    --form                                                      \
+                    --field="PDF : " ""    \
+                    )
+                PDF=$(echo "$BURST" | cut -d "|" -f 1 )
+                pdftk $PDF burst;;
+            "4") pdftk $PDF1 $PDF2 output merged.pdf
+                yad --text="Documentos $PDF1 e $PDF2 Mesclados!" --text-align=center;;
         esac
 done
